35f95eca30b0ebd978afc62a44e7193bc2df5487
kzt206 <kzt206@gmail.com>
2024-07-05 07:22:59 +0000

    Step 6:singlue +/-

--- 9cc.c
+++ 9cc.c
@@ -174,6 +174,7 @@ Node *new_num(int val){
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -190,19 +191,30 @@ Node *expr(){
     }
 }
 
-// mul = primary ("*" primary | "/" primary )*
+// mul = unary ("*" unary | "/" unary )*
 Node *mul(){
-    Node *node = primary();
+    Node *node = unary();
 
     for(;;){
         if(consume('*'))
-         node = new_binary(ND_MUL,node,primary());
+         node = new_binary(ND_MUL,node,unary());
         else if(consume('/'))
-         node = new_binary(ND_DIV,node,primary());
+         node = new_binary(ND_DIV,node,unary());
         else
          return node;
     }
 }
+
+// unary = ("+" | "-")? unary
+//         | primary
+Node *unary(){
+    if(consume('+'))
+     return unary();
+    if(consume('-'))
+     return new_binary(ND_SUB, new_num(0),unary());
+    return primary();
+}
+
 
 // primary = "(" expr ")" | num
 Node *primary(){
@@ -214,6 +226,7 @@ Node *primary(){
 
     return new_num(expect_number());
 }
+
 
 //
 // Code generator
